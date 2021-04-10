56dcbe1e896ecac385a840fc4bbe90c21a64f52e
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-04-10 21:31:15 +0300

    Create hobby.model.php

---
+++ hobby.model.php
@@ -0,0 +1,218 @@
+<?php
+
+require_once('database.class.php');
+
+class Hobby
+{
+    private $firstName;
+    private $lastName;
+    private $dob;
+    private $gender;
+    private $games;
+    private $email;
+    private $phone;
+    private $password;
+    private $image;
+
+    public function __construct(
+        $firstName = null,
+        $lastName = null,
+        $dob = null,
+        $gender = null,
+        $games = null,
+        $email = null,
+        $phone = null,
+        $password = null,
+        $image = null
+    ) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->dob = $dob;
+        $this->gender = $gender;
+        $this->games = $games;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->password = $password;
+        $this->image = $image;
+    }
+
+    /**
+     * Get the value of firstName
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @return  self
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @return  self
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dob
+     */
+    public function getDob()
+    {
+        return $this->dob;
+    }
+
+    /**
+     * Set the value of dob
+     *
+     * @return  self
+     */
+    public function setDob($dob)
+    {
+        $this->dob = $dob;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of gender
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Set the value of gender
+     *
+     * @return  self
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of games
+     */
+    public function getGames()
+    {
+        return $this->games;
+    }
+
+    /**
+     * Set the value of games
+     *
+     * @return  self
+     */
+    public function setGames($games)
+    {
+        $this->games = $games;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of phone
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set the value of phone
+     *
+     * @return  self
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set the value of image
+     *
+     * @return  self
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
